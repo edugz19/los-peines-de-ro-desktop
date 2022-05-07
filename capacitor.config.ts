8ad896c4ea859abc611983c258b21f96ebc7c6e2
Edu García <edugz19@outlook.es>
2022-05-07 13:47:09 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'los-peines-de-ro-desktop',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
